b2af753653969c451649685241d044141601c348
Enyinna Ochulor <enyinnaochulor@gmail.com>
2017-09-11 15:44:55 -0500

    Initial Commit

---
+++ CrackingArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrackingArray
+//
+//  Created by Ochulor, Enyinna on 9/11/17.
+//  Copyright © 2017 Ochulor, Enyinna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
